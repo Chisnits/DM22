330e1532286868fd326e1efd2b5d61f8c91b4964
Chase Derr <Chisnits@users.noreply.github.com>
2017-04-14 10:06:00 -0600

    day5 morning

---
+++ week1/day2/lego-fighter/animate.js
@@ -0,0 +1,7 @@
+$("#reset-button").click(function() {
+  $('.lego-batman').toggleClass('lego-batman-active');
+console.log('clicked')
+});
+$("#reset-button").click(function() {
+  $('.lego-bane').toggleClass('lego-bane-active');
+});
