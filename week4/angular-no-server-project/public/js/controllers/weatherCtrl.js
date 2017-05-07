49d042b44459a228d70bd2fce6263d615bf3d337
Chase Derr <Chisnits@users.noreply.github.com>
2017-05-07 14:01:45 -0600

    displaying current weather according to lat/lon

--- week4/angular-no-server-project/public/js/controllers/weatherCtrl.js
+++ week4/angular-no-server-project/public/js/controllers/weatherCtrl.js
@@ -14,8 +14,15 @@ weatherService.getCurrentLocation().then(function(location){
     weatherService.getWeather($scope.lat, $scope.lon).then(function(weather){
         $scope.weather = weather;
         console.log(weather)
-    })
+        // console.log(weather.weather.pop())
+        var weatherObj = weather.weather.pop()
+        var currentWeather = weatherObj.description;
+        $scope.currentWeather = currentWeather;
+        console.log(currentWeather)
+    }) 
 });
+
+
 
 
 
